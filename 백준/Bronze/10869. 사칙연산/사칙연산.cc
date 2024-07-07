d9854ab25acb61072413ceb7ffd72185afa846b8
Ohhyuntaek <61840656+Ohhyuntaek@users.noreply.github.com>
2024-07-07 21:42:43 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(void) 
+{
+    int a;
+    int b;
+    cin >> a >> b;
+    cout.precision(10);
+    
+    cout << a + b << endl;
+    cout << a - b << endl;
+    cout << a * b << endl;
+    cout << a / b << endl;
+    cout << a % b << endl;
+
+}
