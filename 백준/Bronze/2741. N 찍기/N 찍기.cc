2f301c4567791ebab3ef979a7330d155a06ffbe1
Ohhyuntaek <61840656+Ohhyuntaek@users.noreply.github.com>
2024-08-19 23:24:36 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int a = 0;
+    cin >> a;
+
+    for (int i = 1; i <= a; i++)
+    {
+        cout << i << "\n";
+    }
+
+    return 0;
+}
