bb1bc624c6bc82468271ac7a6a58c65bdffaf506
Ohhyuntaek <61840656+Ohhyuntaek@users.noreply.github.com>
2024-08-19 23:06:12 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int a = 0;
+    cin >> a;
+
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
