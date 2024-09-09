f8a650d923442fab09caf9c15debbe6eb3bc9708
Ohhyuntaek <61840656+Ohhyuntaek@users.noreply.github.com>
2024-09-09 21:42:31 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int A, B, V;
+    cin >> A >> B >> V;
+
+    int days = (V - B) / (A - B);
+
+    if ((V - B) % (A - B) != 0) {
+        days++;
+    }
+
+    cout << days << endl;
+
+    return 0;
+}
